e3a60e5efacd3e342fe88cb8dbf84df819f5f080
Maftun Hashimli <maftunhashimli@gmail.com>
2023-04-11 19:00:30 +0300

    add longestSubarrayWithDistinctEntries functionality

--- src/main/java/src/ds/Array/Subarray.java
+++ src/main/java/src/ds/Array/Subarray.java
@@ -60,4 +60,23 @@ public class Subarray {
         }
         return result;
     }
+
+    public static int longestSubarrayWithDistinctEntries(List<Integer> A) {
+// Records the most recent occurrences of each entry.
+        Map<Integer, Integer> mostRecentOccurrence=new HashMap<>();
+        int longestDupFreeSubarrayStartldx=0, result=0;
+        for (int i=0; i<A.size(); ++i) {
+            Integer dupldx=mostRecentOccurrence.put(A.get(i), i);
+// A.get(i) appeared before. Did it appear in the longest current
+// subarray?
+            if (dupldx!=null) {
+                if (dupldx>=longestDupFreeSubarrayStartldx) {
+                    result=Math.max(result, i-longestDupFreeSubarrayStartldx);
+                    longestDupFreeSubarrayStartldx=dupldx+1;
+                }
+            }
+        }
+        result=Math.max(result, A.size()-longestDupFreeSubarrayStartldx);
+        return result;
+    }
 }
