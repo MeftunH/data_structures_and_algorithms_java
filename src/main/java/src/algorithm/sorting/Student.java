1d9107527418bdc45f9ff932444e42e7006f7e31
Maftun Hashimli <maftunhashimli@gmail.com>
2023-04-11 19:06:42 +0300

    add Student class

---
+++ src/main/java/src/algorithm/sorting/Student.java
@@ -0,0 +1,30 @@
+package src.algorithm.sorting;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Student implements Comparable<Student> {
+    public String name;
+    public double gradePointAverage;
+
+    Student(String name, double gradePointAverage) {
+        this.name=name;
+        this.gradePointAverage=gradePointAverage;
+    }
+
+    public static void sortByGPA(List<Student> students) {
+        Collections.sort(
+                students, Collections.reverseOrder(new Comparator<Student>() {
+                    @Override
+                    public int compare(Student a, Student b) {
+                        return Double.compare(a.gradePointAverage, b.gradePointAverage);
+                    }
+                }));
+    }
+
+    public int compareTo(Student that) {
+        return name.compareTo(that.name);
+    }
+}
