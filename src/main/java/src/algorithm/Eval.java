6bdf9145ede7d7416381aa1972b64fd06aebb803
Maftun Hashimli <maftunhashimli@gmail.com>
2023-04-15 23:51:47 +0300

    add isWellFormedABoolean

--- src/main/java/src/algorithm/Eval.java
+++ src/main/java/src/algorithm/Eval.java
@@ -35,4 +35,24 @@ public class Eval {
         }
         return intermediateResults.removeFirst();
     }
+
+    public static boolean isWellFormedABoolean(String s) {
+        Deque<Character> leftChars=new LinkedList<>();
+        for (int i=0; i<s.length(); ++i) {
+            if (s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='[') {
+                leftChars.addFirst(s.charAt(i));
+            } else {
+                if (leftChars.isEmpty()) {
+                    return false; // Unmatched right char.
+                }
+                if ((s.charAt(i)==')'&&leftChars.peekFirst()!='(')
+                        ||(s.charAt(i)==')'&&leftChars.peekFirst()!='{')
+                        ||(s.charAt(i)==']'&&leftChars.peekFirst()!='[')) {
+                    return false; // Mismatched chars.
+                }
+                leftChars.removeFirst();
+            }
+        }
+        return leftChars.isEmpty();
+    }
 }
